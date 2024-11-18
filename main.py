871079ada1dc9d9d28fde82e61e1bd631a0e022b
Gitin18 <inka25618@gmail.com>
2024-11-18 03:22:14 +0100

    new code

--- main.py
+++ main.py
@@ -4,9 +4,26 @@ class User():
         self.name = name
         self.account = account
 
-class Admin (User):
-    def __init__(self,id,name,account, adm_account):
-        super().__init__(id,name,account)
-        self.adm_account =  []
 
-    
+
+class Admin (User):
+    def __init__(self,id, name, account, adm_account):
+        super().__init__(id, name, account)
+        self.adm_account = {}
+
+    def add_user (self):
+        self.adm_account[self.name]="User account"
+        print(f"{self.name} got an account")
+
+    def remove_user(self):
+        if self.name in self.adm_account:
+            del self.adm_account[self.name]
+            print(f"{self.name} was removed from the list")
+        else:
+            print(f"{self.name}is not found in the list")
+user1 = User( 137, "Smith", "137A")
+
+
+
+
+
